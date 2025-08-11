00bdf5c5f8431105d5f9eb7bfd49e8fa2bfe500c
Darkmen203 <dima-pivovarov03@mail.ru>
2025-08-11 13:11:41 +0300

    added modal with email user

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 import { User } from './interfaces/interfaces';
 import { userList } from './users-list';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('find_user_test');
 
-  public search_name: string = "";
-
-  public filter: string = "all";
-
+  public search_name = '';
+  public filter = 'all';
   public render_users: User[] = userList;
-
   private readonly users_list: User[] = userList;
+
+  public modalOpen = false;
+  public selectedEmail: string | null = null;
 
   onChangeSearch() {
     const base = this.filter === 'all'
@@ -30,5 +33,15 @@ export class App {
     this.render_users = q
       ? base.filter(u => u.name.toLowerCase().startsWith(q))
       : base;
+  }
+
+  openModal(user: User) {
+    this.selectedEmail = user.email;
+    this.modalOpen = true;
+  }
+
+  closeModal() {
+    this.modalOpen = false;
+    this.selectedEmail = null;
   }
 }
