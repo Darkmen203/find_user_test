7e5e83b9f3e98418be5458d48996d3f76596a42c
Darkmen203 <dima-pivovarov03@mail.ru>
2025-08-11 12:20:28 +0300

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { User } from './interfaces/interfaces';
+import { userList } from './users-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('find_user_test');
+
+  public search_name: string = "";
+
+  public filter: string = "all";
+
+  public render_users: User[] = userList;
+
+  private readonly users_list: User[] = userList;
+
+  onChangeSearch() {
+    const base = this.filter === 'all'
+      ? this.users_list
+      : this.users_list.filter(u => this.filter === 'active' ? u.active : !u.active);
+
+    const q = this.search_name.trim().toLowerCase();
+
+    this.render_users = q
+      ? base.filter(u => u.name.toLowerCase().startsWith(q))
+      : base;
+  }
 }
